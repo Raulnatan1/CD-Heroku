1ac2e9b2dcb32c643ec785c951c795f0fe1b207a
Raulnatan1 <70021349+Raulnatan1@users.noreply.github.com>
2020-10-09 10:57:58 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Raul Natan Martins Da Silva"
+    return "Olá, seja bem vindo"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
